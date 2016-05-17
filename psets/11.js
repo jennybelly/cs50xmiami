f7a8f47f39804c0775d5ad5cec280fd512de04ce
Jennifer Dellis <jenniferdellis@Jennifers-MacBook-Air.local>
2016-05-17 19:59:33 -0400

    Add completed pset11

--- psets/11.js
+++ psets/11.js
@@ -12,3 +12,14 @@ Write a function named calculateAge that:
 */
 
 // write your solution here...
+var current_year = new Date().getFullYear();
+
+function calculateAge(birth_year, current_year) {
+  var age_one = current_year - birth_year;
+  var age_two = current_year - birth_year + 1;
+  console.log("You are either " + age_one + " or " + age_two + ".");
+}
+
+calculateAge(1982, current_year);
+calculateAge(1991, current_year);
+calculateAge(1948, current_year);
