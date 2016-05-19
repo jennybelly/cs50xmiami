c709fcb09e121117e6b9ef4e2ec19bd0ac9ad0b3
Jennifer Dellis <jenniferdellis@Jennifers-MacBook-Air.local>
2016-05-19 14:56:57 -0400

    Add completed pset18

--- psets/18.js
+++ psets/18.js
@@ -7,3 +7,11 @@ The even/odd reporter
 */
 
 // write your solution here...
+for (i = 0; i < 21; i++) {
+  if ((i % 2) == 1) {
+    console.log("" + i + " is odd.");
+  }
+  else {
+    console.log("" + i + " is even.");
+  }
+}
