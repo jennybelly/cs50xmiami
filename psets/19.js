1618c91a1131ef0d35d8eae8fbc6c2a1a6c580db
Jennifer Dellis <jenniferdellis@Jennifers-MacBook-Air.local>
2016-05-19 15:08:57 -0400

    Add completed pset19

--- psets/19.js
+++ psets/19.js
@@ -9,3 +9,7 @@ Write a for loop that will iterate from 0 to 10.
 */
 
 // write your solution here...
+for (i = 0; i < 11; i++) {
+  var NN = i * 9;
+  console.log("" + i + " * 9 = " + NN + ".");
+}
